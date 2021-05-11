35561cd6f1663a41d80830a910a7602008562e9e
Siirko <Siirko@github.com>
2021-05-11 17:24:16 +0200

    Modification de la classe Graphe et du diagrame de conception

--- src/main/java/Fourmis/Graphe.java
+++ src/main/java/Fourmis/Graphe.java
@@ -1,4 +1,22 @@
 package Fourmis;
 
+import java.util.List;
+
 public class Graphe {
+    private Integer width;
+    private Integer height;
+
+    public Graphe(Integer width, Integer height){
+        /*
+        for(Noeud i = 0; i < width * height; i++){
+
+        }
+         */
+    }
+
+    public Noeud getNoeud(Integer row, Integer column){
+        Noeud g = new Noeud();
+        return g;
+    }
+
 }
