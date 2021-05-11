8f7020268a4549099d5361eb08882b4185724bab
Siirko <Siirko@github.com>
2021-05-11 17:54:23 +0200

    Complétation de la classe Graphe

--- src/main/java/Fourmis/Graphe.java
+++ src/main/java/Fourmis/Graphe.java
@@ -5,18 +5,24 @@ import java.util.List;
 public class Graphe {
     private Integer width;
     private Integer height;
+    private Noeud[][] graphe_tab;
 
     public Graphe(Integer width, Integer height){
-        /*
-        for(Noeud i = 0; i < width * height; i++){
-
+        graphe_tab = new Noeud[width][height];
+        for(int i = 0; i < graphe_tab.length; i++){
+            for(int j = 0; j < graphe_tab[i].length; j++){
+                Noeud n = new Noeud();
+                graphe_tab[i][j] = n;
+            }
         }
-         */
     }
 
     public Noeud getNoeud(Integer row, Integer column){
-        Noeud g = new Noeud();
-        return g;
+        return graphe_tab[row][column];
     }
 
+    public void putObstacle(Integer row, Integer column){
+        Noeud n = graphe_tab[row][column];
+        n.setFree(false);
+    }
 }
