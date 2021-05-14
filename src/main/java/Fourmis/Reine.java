1c38083d9f6f051ca014a6e38fd21b33acbce37e
Siirko <Siirko@github.com>
2021-05-14 13:03:40 +0200

    Retour en arrière (classe Reine)

--- src/main/java/Fourmis/Reine.java
+++ src/main/java/Fourmis/Reine.java
@@ -2,12 +2,12 @@ package Fourmis;
 
 public class Reine extends Fourmis{
     //Avec ça qui prend le position du noeud de la reine
-    Noeud emplacements;
+   // private Reine r;
 
     public Reine(Noeud noeud) {
         super(noeud);
-        this.emplacements = noeud;
         noeud.setNoeudState(Noeud.STATE.ANTHILL);
+       // this.r = new Reine(noeud);
     }
 
     @Override
@@ -15,12 +15,9 @@ public class Reine extends Fourmis{
     }
 
 
-    //Puis créé la fourmis soldier à l'emplacement de la reine, donc à la fourmillière
     public void createSoldiers(int amount) {
         for(int i = 0; i<amount; i++){
             Soldat soldat = new Soldat(this.getPosition());
-            //pour créé le soldat sur l'emplacement de la reine (donc la fourmillière
-            soldat.setPosition(emplacements);
         }
     }
 }
