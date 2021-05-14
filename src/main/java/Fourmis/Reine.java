ff4826a59a0f9113f34d2cd5ba4b3e413afe3b46
Siirko <Siirko@github.com>
2021-05-14 12:00:00 +0200

    Implémentation de createSoldiers

--- src/main/java/Fourmis/Reine.java
+++ src/main/java/Fourmis/Reine.java
@@ -1,9 +1,12 @@
 package Fourmis;
 
 public class Reine extends Fourmis{
+    //Avec ça qui prend le position du noeud de la reine
+    Noeud emplacements;
 
     public Reine(Noeud noeud) {
         super(noeud);
+        this.emplacements = noeud;
         noeud.setNoeudState(Noeud.STATE.ANTHILL);
     }
 
@@ -11,10 +14,13 @@ public class Reine extends Fourmis{
     public void move() {
     }
 
-    //A VOIR !
+
+    //Puis créé la fourmis soldier à l'emplacement de la reine, donc à la fourmillière
     public void createSoldiers(int amount) {
         for(int i = 0; i<amount; i++){
             Soldat soldat = new Soldat(this.getPosition());
+            //pour créé le soldat sur l'emplacement de la reine (donc la fourmillière
+            soldat.setPosition(emplacements);
         }
     }
 }
