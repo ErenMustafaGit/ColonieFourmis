b3fac6ac55fcc3acbabc25cffbcf0dbbcc7f1114
Eren <pro.erenmustafa.ceylan@gmail.com>
2021-05-19 10:32:01 +0200

    ReineTest.java : move() + reateSoldiersMove()

--- src/test/java/Fourmis/ReineTest.java
+++ src/test/java/Fourmis/ReineTest.java
@@ -12,28 +12,47 @@ class ReineTest {
     AntFacadeController appli;
     static final int WIDTH = 3;
     static final int HEIGHT = 4;
+    Graphe graphe;
 
     @BeforeEach
     void setUp() {
         appli = new ControlFourmis();
         appli.createGrid(WIDTH, HEIGHT);
+        graphe = new Graphe(WIDTH, HEIGHT);
     }
 
     @Test
     @DisplayName(("Reine qui ne doit pas se déplacer"))
     void move() {
-        appli.createColony(1,2);
-        BitSet[][] bitset = appli.play(5,false);
-        BitSet actual = bitset[1][2];
-        assertTrue(actual.get(0), "Fourmillière ne s'est pas déplacé " + actual);
+        Reine reine = new Reine(graphe.getNoeud(0,0));
+        reine.move();
+        assertEquals(graphe.getNoeud(0,0), reine.getPosition());
     }
 
     @Test
     void createSoldiers() {
+
+
         appli.createColony(1,2);
         appli.createSoldiers(5);
         BitSet[][] bitset = appli.play(0,false);
         BitSet actual = bitset[1][2];
-        assertTrue(actual.get(2), "Il n'y a pas de soldat : " + actual);
+        assertTrue(actual.get(2), "Il y a des soldats : " + actual);
+    }
+
+    @Test
+    @DisplayName("Les soldats qu'une colonie crée, bouge bien")
+    void createSoldiersMove() {
+
+        appli.createColony(0,0);
+        appli.putObstacle(1,0);
+
+        appli.createSoldiers(1);
+        BitSet[][] bitset = appli.play(1,false);
+        BitSet right = bitset[0][1];
+        BitSet down = bitset[1][0];
+        assertTrue(right.get(2), "Soldat a bouger : " + right);
+        assertFalse(down.get(2), "Soldat n'a pas bouger dans l'obstacle : " + down);
+
     }
 }
