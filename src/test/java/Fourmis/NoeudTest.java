3e5e5fb176d046337abdeb2161d2aa9d82baa007
Siirko <Siirko@github.com>
2021-05-19 09:51:44 +0200

    Complétation du test unitaire de la classe Noeud.

--- src/test/java/Fourmis/NoeudTest.java
+++ src/test/java/Fourmis/NoeudTest.java
@@ -1,8 +1,11 @@
 package Fourmis;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,7 +28,6 @@ class NoeudTest {
         g.createColony(2,2);
         g.putObstacle(0,1);
         g.putObstacle(1,0);
-
     }
 
 
@@ -35,15 +37,55 @@ class NoeudTest {
     @Test
     @DisplayName(("Etat du noeud"))
     void testStateNoeud(){
-        String etat_colonnie = g.getNoeud(2,2).getNoeudState().toString();
-        String etat_obstacle = g.getNoeud(0,1).getNoeudState().toString();
-        assertEquals("ANTHILL", etat_colonnie);
-        assertEquals("OBSTACLE", etat_obstacle);
+        Noeud.STATE etat_colonnie = g.getNoeud(2,2).getNoeudState();
+        Noeud.STATE etat_obstacle = g.getNoeud(0,1).getNoeudState();
+        Noeud.STATE etat_free = g.getNoeud(0,10).getNoeudState();
+        assertEquals(Noeud.STATE.ANTHILL, etat_colonnie);
+        assertEquals(Noeud.STATE.OBSTACLE, etat_obstacle);
+        assertEquals(Noeud.STATE.FREE, etat_free);
     }
 
     @Test
-    @DisplayName(("Voisin disponible d'un noeud"))
-    void testVoinsinNoeud(){
+    @DisplayName(("Changement d'état d'un du noeud"))
+    void testSetStateNoeud(){
+        g.getNoeud(4,4).setNoeudState(Noeud.STATE.ANTHILL);
+        Noeud.STATE etat = Noeud.STATE.ANTHILL;
+        assertEquals(Noeud.STATE.ANTHILL, etat);
+    }
+
+    @Test
+    @DisplayName(("Ajout d'un noeud voisin à un noeud"))
+    void testAddNoeudVoisin(){
+        Noeud n = new Noeud();
+        Noeud n_voisin = new Noeud();
+        n.addNoeudVoisin(n_voisin);
+        n_voisin.addNoeudVoisin(n);
+
+        boolean n_voisin_de_nvoisin= false;
+        List<Noeud> voisins = n.getVoisins();
+        for(Noeud noeud : voisins){
+            if(noeud.equals(n_voisin)){
+                n_voisin_de_nvoisin = true;
+                break;
+            }
+        }
+
+        boolean nvoisin_de_n = false;
+        List<Noeud> nvoisins = n_voisin.getVoisins();
+        for(Noeud noeud : nvoisins){
+            if(noeud.equals(n)){
+                nvoisin_de_n = true;
+                break;
+            }
+        }
+
+        assertTrue(n_voisin_de_nvoisin);
+        assertTrue(nvoisin_de_n);
+    }
+
+    @Test
+    @DisplayName(("Voisins disponible d'un noeud"))
+    void testFreeVoinsinNoeud(){
         //Pas de voisin car le noeud en 0,0 est encerclé
         Noeud n = g.getNoeud(0,0);
         int nb = n.getFreeVoisins().size();
@@ -53,6 +95,28 @@ class NoeudTest {
         Noeud n2 = g.getNoeud(4,4);
         int nb2 = n2.getFreeVoisins().size();
         assertEquals(4, nb2);
+    }
+
+    @Test
+    @DisplayName(("Voisins d'un noeud"))
+    void testVoisinNoeud(){
+        Noeud n = g.getNoeud(0,0);
+        List<Noeud>Voisin = n.getVoisins();
+        Noeud n1 = g.getNoeud(0,1);
+        Noeud n2 = g.getNoeud(1,0);
+
+        boolean voisin_droite = false;
+        boolean voisin_bas = false;
+        for(Noeud noeud : Voisin){
+            if(noeud.equals(n1)){
+                voisin_droite = true;
+            }
+            else if (noeud.equals(n2)){
+                voisin_bas = true;
+            }
+        }
+        assertTrue(voisin_bas);
+        assertTrue(voisin_droite);
     }
 
 }
