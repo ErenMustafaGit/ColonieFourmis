4342b0a064d28798f34cd5bb619e1a0b1adb55ab
Eren <pro.erenmustafa.ceylan@gmail.com>
2021-05-19 14:30:14 +0200

    Ajout de la class Worker

---
+++ src/main/java/Fourmis/Worker.java
@@ -0,0 +1,18 @@
+package Fourmis;
+
+public class Worker extends Ant {
+
+    /**
+     * Créé une fourmis à un noeud donné
+     *
+     * @param node : emplacement de la fourmis
+     */
+    public Worker(Node node) {
+        super(node);
+    }
+
+    @Override
+    public void move() {
+
+    }
+}
