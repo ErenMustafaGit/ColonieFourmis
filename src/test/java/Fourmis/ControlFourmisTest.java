85827480a8b8ace123836cab156dd090c247ab7f
Siirko <Siirko@github.com>
2021-05-19 10:27:06 +0200

    Ajout du test unitaire de ControlFourmis (ControlFourmisTest)

---
+++ src/test/java/Fourmis/ControlFourmisTest.java
@@ -0,0 +1,32 @@
+package Fourmis;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControlFourmisTest {
+
+    @Test
+    void createGrid() {
+    }
+
+    @Test
+    void putObstacle() {
+    }
+
+    @Test
+    void createColony() {
+    }
+
+    @Test
+    void createSoldiers() {
+    }
+
+    @Test
+    void createWorkers() {
+    }
+
+    @Test
+    void play() {
+    }
+}
