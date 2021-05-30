1096531d533abf4f30c77e68a07a75c4291b87d8
Eren <pro.erenmustafa.ceylan@gmail.com>
2021-05-30 18:18:04 +0200

    ScenarioTest : SetUp + test a et b reussi

--- src/test/java/Fourmis/ScenarioTest.java
+++ src/test/java/Fourmis/ScenarioTest.java
@@ -4,26 +4,42 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.BitSet;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ScenarioTest {
 
     static final int WIDTH = 3;
     static final int HEIGHT = 3;
-    static final int FOODPARAM = 20;
-    static final int EVAPORATIONPARAM = 1;
-    static final int PHEROMONEPARAM = WIDTH*EVAPORATIONPARAM;
+    static final int FOODQUANTITY = 6000;
+    static final int FOODPARAM = 1;
+    static final int EVAPORATIONPARAM = 0;
+    static final int PHEROMONEPARAM = 0;
     AntFacadeController appli;
 
     @BeforeEach
     void setUp()
     {
         appli = new ControlAnt();
+
         appli.createGrid(WIDTH, HEIGHT);
-        appli.createColony(0,0);
+
+        appli.putObstacle(0,0);
+        appli.createColony(1,0);
+        appli.putObstacle(2,0);
+
+        Pheromone pheromone2 = new Pheromone(2, null);
+        Graph graph = ((ControlAnt)appli).getGraph();
+        graph.getNoeud(0,1).addPheromone(pheromone2);
+
+        Pheromone pheromone1 = new Pheromone(1, null);
+        graph.getNoeud(1,2).addPheromone(pheromone1);
+
+        appli.putFood(0,2, FOODQUANTITY);
+        appli.putFood(2,2,FOODQUANTITY);
 
 
-        appli.createSoldiers(1);
         appli.createWorkers(1);
-
         appli.setParameters(EVAPORATIONPARAM, FOODPARAM, PHEROMONEPARAM);
     }
 
@@ -31,5 +47,50 @@ public class ScenarioTest {
     @DisplayName("Scenario de test 2")
     void test1(){
 
+        //Situation initiale
+        BitSet[][] bitSets = appli.play(0,false);
+
+        //Noeud où il y a la fourmillière
+        BitSet bitSet = bitSets[1][0];
+        //Si il se trouve bien une fourmillière
+        assertTrue(bitSet.get(0));
+        //Et une ouvrière sans Nourriture
+        assertTrue(bitSet.get(3));
+
+        //Noeud où il y a les phéromones, Test si ils sont bien présent
+        bitSet = bitSets[0][1];
+        assertTrue(bitSet.get(6));
+        bitSet = bitSets[1][2];
+        assertTrue(bitSet.get(6));
+
+        //Noeud où il y a les obstacles, Test si ils sont bien présent
+        bitSet = bitSets[0][0];
+        assertTrue(bitSet.get(1));
+        bitSet = bitSets[2][0];
+        assertTrue(bitSet.get(1));
+
+        //Noeud où il y a les nourritures, Test si ils sont bien présent
+        bitSet = bitSets[0][2];
+        assertTrue(bitSet.get(5));
+        bitSet = bitSets[2][2];
+        assertTrue(bitSet.get(5));
+
+
+        //Tour 1
+        bitSets = appli.play(1,false);
+
+        //a) Mouvement en 1,1 de l'ouvrière attendu
+        bitSet = bitSets[1][1];
+        assertTrue(bitSet.get(3));
+
+
+        //Tour 2
+        bitSets = appli.play(1,false);
+
+        //b) 3 choix de mouvement : 0,1 | 1,2 | 2,1  de l'ouvrière attendu
+        boolean goodMove = false;
+        if(bitSets[0][1].get(3) || bitSets[1][2].get(3) ||bitSets[2][1].get(3))
+            goodMove = true;
+        assertTrue(goodMove);
     }
 }
