ea3cab4243b0663886d0bb0406a133f5bdce051c
Siirko <Siirko@github.com>
2021-06-04 13:14:21 +0200

    Ajout des commentaires nécessaire, et ajout de la fonction getIndexAlgorithme dans le diagrame de conception

--- src/main/java/Fourmis/State.java
+++ src/main/java/Fourmis/State.java
@@ -1,5 +1,11 @@
 package Fourmis;
 
+/**
+ * Différent état possible sur un noeud
+ * FREE == Libre
+ * OBSTACLE == Obstacle
+ * ANTHILL == Fourmillière
+ */
 public enum State {
     FREE,
     OBSTACLE,
